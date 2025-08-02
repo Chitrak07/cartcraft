2f822d4629f95156d7e0c5385e7f959919840eff
Chitrak07 <deepak119907@gmail.com>
2025-08-03 05:03:58 +0530

    WIP: Setup PostgreSQL and verified app is running

---
+++ cartcraft-backend/src/main/java/com/cartcraft/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.cartcraft.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import  org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        String s = "CraftCart backend is up!";
+        return s;
+    }
+
+}
